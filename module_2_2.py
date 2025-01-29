15d966ed468ae7aaf0e8c1025a7a6d82f4fd9b7b
olegBelg <kniginol@mail.ru>
2025-01-29 14:07:18 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first and second == third:
+    print("3")
+elif first == second != third:
+    print("2")
+elif first != second == third:
+    print("2")
+elif first == third != second :
+    print("2")
+elif first != third != second :
+    print("0")
